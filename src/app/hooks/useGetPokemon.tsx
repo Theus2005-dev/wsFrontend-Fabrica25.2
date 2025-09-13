39a6c2e8fb16074b5635cabf7ee58116d0b9d923
Theus2005-dev <matheusarcangelli36@gmail.com>
2025-09-13 09:08:32 -0300

    feat: Criação do hook para consumir api

--- src/app/hooks/useGetPokemon.tsx
+++ src/app/hooks/useGetPokemon.tsx
@@ -1,25 +1,32 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
 import { usePokemonType } from "../types/pokemonType";
 
-export  default function useGetPokemons(){
-        const [data, setData] = useState<usePokemonType[]>([])
-        const [loading , setLoading] = useState(true);
+export default function useGetPokemons() {
+  const [data, setData] = useState<usePokemonType[]>([]);
+  const [loading, setLoading] = useState(true);
 
-        useEffect(()=>{
-               async function getData(){
-                try{
-                     const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-                     const data = await response.json() 
-                     setData(data.results);  
-                } catch(err){
-                        console.error(err);
-                }finally{
-                        setLoading(false);
-                }
-                        
-               } 
-               getData();
-        },[])
-        return{ data, loading};
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+        const data = await response.json();
+        const pokemonInfo = data.results.map((pokemon: any, index: number) => {
+          const PokemonId = pokemon.url.split("/").filter(Boolean).pop();
+          return {
+            id: Number(PokemonId),
+            name: pokemon.name,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${PokemonId}.png`,
+          };
+        });
+        setData(pokemonInfo);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData();
+  }, []);
+  return { data, loading };
 }

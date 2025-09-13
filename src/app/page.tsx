c0813ac2a2d59e85ee1ae536e720cbd690131d9f
Theus2005-dev <matheusarcangelli36@gmail.com>
2025-09-13 11:11:32 -0300

    chore: add DataView, DataProvider e searchBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,23 @@ import GetPokemons from "./hooks/useGetPokemon";
 import useGetPokemons from "./hooks/useGetPokemon";
 import { useEffect } from "react";
 import Header from "@/components/Header";
-
-
+import SearchBar from "@/components/SearchBar";
+import DataProvider, { useData } from "@/context/dataContext";
+import DataView from "@/components/DataView";
 export default function Home() {
-  const {data, loading} = useGetPokemons();
+  const {filtered} = useData();
   useEffect(()=>{
-    console.log(data)
-  })
+    console.log(filtered)
+  },[filtered])
   return (
    <>
-      <header>
+      <header className="flex bg-blue-200 justify-between">
          <Header/>
+         <SearchBar/>
       </header>
+      <main>
+        <DataView />
+      </main>
    </>
   );
 }

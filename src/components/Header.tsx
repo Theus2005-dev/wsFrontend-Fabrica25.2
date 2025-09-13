f8df6a3eff88e0b1d14117dac5d27150edff857c
Theus2005-dev <matheusarcangelli36@gmail.com>
2025-09-13 09:59:28 -0300

    feat: criação componente header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import StarIcon from "@/icons/starIcon";
+export default function Header() {
+  return (
+    <header className="flex bg-blue-200 p-2">
+      <h1 className="text-3xl">Fábrica Software - UNIPE</h1>
+      <StarIcon/>
+    </header>
+  );
+}

ab5e17101b5ff79b36e2359ebd6009ea1060fb55
Theus2005-dev <matheusarcangelli36@gmail.com>
2025-09-13 10:00:17 -0300

    feat: chamada de componentes na page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,19 @@ import Image from "next/image";
 import GetPokemons from "./hooks/useGetPokemon";
 import useGetPokemons from "./hooks/useGetPokemon";
 import { useEffect } from "react";
+import Header from "@/components/Header";
+
+
 export default function Home() {
   const {data, loading} = useGetPokemons();
   useEffect(()=>{
     console.log(data)
   })
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
-      </main>
-      
-       
-    </div>
+   <>
+      <header>
+         <Header/>
+      </header>
+   </>
   );
 }

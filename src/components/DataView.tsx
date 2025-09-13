594977ce28566bbd6d62d82ac985233027874119
Theus2005-dev <matheusarcangelli36@gmail.com>
2025-09-13 11:08:59 -0300

    feat: criação do componente para mostrar os dados

---
+++ src/components/DataView.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { useData } from "@/context/dataContext";
+export default function DataView(){
+    const {viewMode, setViewMode, filtered, loading} = useData();
+    
+
+    if(viewMode === 'grid'){
+        return (
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {loading && <p>Carregando...</p>}
+                {filtered.length > 0 &&(
+                    filtered.map((e)=>(
+                        <div className="bg-blue-100 rounded-sm mt-2 text-center" key={e.id}>
+                            <img src={e.image} alt="imagem pokemon" className="w-30 h-50 rounded-sm ml-5 sm:ml-30" />
+                            <strong>Nome: {e.name}</strong>
+                            <p>Id: {e.id}</p>
+                        </div>
+                    ))
+                )}
+            </div>
+        )
+    }
+}
